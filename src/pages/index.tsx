b81be30ebdd32d79fbc580e42aa58a8ef054d593
tnicacio <nicacio.t92@gmail.com>
2021-04-05 23:05:36 -0300

    header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,36 @@
-export default function Home() {
+import { Flex, Button, Stack } from '@chakra-ui/react';
+import { Input } from '../components/Form/Input';
+
+export default function SignIn() {
   return (
-    <div>
-      <h1>
-        Dashboard
-      </h1>
-    </div>
+    <Flex
+      w="100vw"
+      h="100vh"
+      align="center"
+      justify="center"
+    >
+      <Flex as="form"
+        width="100%"
+        maxWidth={360}
+        bg="gray.800"
+        p="8" //padding 8 -> /4 = 2rem e *4 = 32px
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Stack spacing="4">
+          <Input type="email" name="email" label="E-mail" />
+          <Input type="password" name="password" label="Senha" />
+        </Stack>
+
+        <Button
+          type="submit"
+          mt="6"
+          colorScheme="pink"
+          size="lg"
+        >
+          Entrar
+        </Button>
+      </Flex>
+    </Flex> 
   )
 }
