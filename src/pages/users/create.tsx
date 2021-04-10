5ed00210b00c917038bff9cab1ea4ea2a7269e6b
tnicacio <nicacio.t92@gmail.com>
2021-04-10 14:01:55 -0300

    adding some navigation links

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -2,6 +2,8 @@ import { Box, Flex, Heading, Divider, VStack, SimpleGrid, HStack, Button } from 
 import { Header } from "../../components/Header";
 import { Sidebar } from "../../components/Sidebar";
 import { Input } from '../../components/Form/Input';
+import React from "react";
+import Link from "next/link";
 
 export default function CreateUser() {
   return (
@@ -10,19 +12,19 @@ export default function CreateUser() {
       <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
         <Sidebar />
         
-        <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+        <Box flex="1" borderRadius={8} bg="gray.800" p={["6","8"]}>
           <Heading size="lg" fontWeight="normal">
             Criar usuário
           </Heading>
           <Divider my="6" borderColor="gray.700"/>
 
           <VStack spacing="8">
-            <SimpleGrid minChildWidth="240px" spacing="8" w="100%"> 
+            <SimpleGrid minChildWidth="240px" spacing={["6","8"]} w="100%"> 
               <Input name="name" label="Nome completo" />
               <Input name="email" type="email" label="E-mail" />
             </SimpleGrid>
 
-            <SimpleGrid minChildWidth="240px" spacing="8" w="100%"> 
+            <SimpleGrid minChildWidth="240px" spacing={["6","8"]} w="100%"> 
               <Input name="password" type="password" label="Senha" />
               <Input name="password_confirmation" type="password" label="Confirmação da senha" />
             </SimpleGrid>
@@ -30,9 +32,11 @@ export default function CreateUser() {
 
           <Flex mt="8" justify="flex-end">
             <HStack spacing="4">
-              <Button colorScheme="whiteAlpha">
-                Cancelar
-              </Button>
+              <Link href="/users" passHref> 
+                <Button as="a" colorScheme="whiteAlpha">
+                  Cancelar
+                </Button>
+              </Link>
               <Button colorScheme="pink">
                 Salvar
               </Button>
